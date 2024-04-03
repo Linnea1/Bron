ef1a770e9122db21987b2d8ffcd6146e62a6f09e
Linnéa <linnea.a.14@gmail.com>
2024-04-03 10:40:21 +0100

    data

--- data.js
+++ data.js
@@ -40,3 +40,46 @@ let CLUES=[
 
     }
 ];
+
+let SUSPECTS=[
+    {
+        id:1,
+        name:"Kristoffer",
+        age:"50",
+        image:"image path",
+        text:"Klas har lånat pengar från Kristoffer och hans ex fru Emma. Kristoffer har bett Klas betala tillbaka summan pengar men han har inte haft råd. Klas och Kristoffer bråkade kvällen innan mordet. Han var hemma med barnen medan frun var borta."
+
+    },
+    {
+        id:2,
+        name:"Göran",
+        age:"57",
+        image:"image path",
+        text:"Klas smsar Göran efter sin stora vinst på casinot. Jobbade för Uber den kvällen, han hade inte mycket körningar den kvällen och sade att han ofta stod parkerad."
+
+    },
+    {
+        id:3,
+        name:"Erik",
+        age:"34",
+        image:"image path",
+        text:"Erik ser Klas vinna den stora mängden pengar. Erik stämplar ut samtidigt som Klas och stämplar in en timme senare."
+
+    },
+    {
+        id:5,
+        name:"Robin",
+        age:"30",
+        image:"image path",
+        text:"Precis kommit ut från fängelset för ett liknande mordförsök, han är också dömd för rånförsök och även andra mindre brott. På gatorna är han en känd knarklangare. Robin säger att han var på en fest i centrala Malmö, däremot kan ingen bekräfta att han var på denna fest."
+
+    },
+    {
+        id:5,
+        name:"Anna",
+        age:"48",
+        image:"image path",
+        text:"Gick hem från en AW och såg Klas räkna massor av kontanter bakom ett träd."
+
+    }
+];
