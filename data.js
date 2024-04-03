2a0fbd35c50907ef9e799274496f00a6409b91e9
Linnéa <linnea.a.14@gmail.com>
2024-04-03 10:28:21 +0100

    json och js struktur

---
+++ data.js
@@ -0,0 +1,42 @@
+let CLUES=[
+    {
+        id:1,
+        code:6435,
+        title:"Titel",
+        image:"image path",
+        text:"Själva ledtråden"
+
+    },
+    {
+        id:2,
+        code:2201,
+        title:"Titel",
+        image:"image path",
+        text:"Själva ledtråden"
+
+    },
+    {
+        id:3,
+        code:9782,
+        title:"Titel",
+        image:"image path",
+        text:"Själva ledtråden"
+
+    },
+    {
+        id:4,
+        code:8414,
+        title:"Titel",
+        image:"image path",
+        text:"Själva ledtråden"
+
+    },
+    {
+        id:5,
+        code:2009,
+        title:"Titel",
+        image:"image path",
+        text:"Själva ledtråden"
+
+    }
+];
