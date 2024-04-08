46abd876eee245997d599a9569c4f89114daac6d
ericalundstrom <erica.lundstrom99@hotmail.com>
2024-04-08 10:26:00 +0200

    upd

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -7,3 +7,21 @@ async function fetching(URL, method, body) {
 
     return response;
 }
+
+
+
+function basicHeader() {
+    let user = JSON.parse(localStorage.getItem("user"));
+
+    document.querySelector("header").innerHTML = ` 
+        <div id="profilePicture" class="icon"></div>
+    `;
+
+    document.querySelector("#profilePicture").style.backgroundImage = `url('Bilder/360_F_303991942_n0GmMYFyNkDGlhvzF6605BSK9mYBXX6B.jpg')`;
+
+    document.querySelector("#profilePicture").addEventListener("click", e => {
+        // newState();
+        RenderUserPage(user);
+    });
+
+}
