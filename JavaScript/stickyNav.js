7f9b813b7da666acdfd8b4edfc2c4e9065b3d3ce
ericalundstrom <erica.lundstrom99@hotmail.com>
2024-04-21 22:05:04 +0200

    upd

--- JavaScript/stickyNav.js
+++ JavaScript/stickyNav.js
@@ -5,19 +5,19 @@ function stickyNav() {
     <ul>
       <li class="nav-home nav-box" onclick="RenderOptions()">
         <i class="fa-solid fa-house"></i>
-        <p class="nav-icon-name text-home">Hem</p>
+        <p class="nav-icon-name text-home"></p>
       </li>
       <li class="nav-suspects nav-box" onclick="RenderSuspects()">
         <i class="fa-solid fa-person"></i>
-        <p class="nav-icon-name text-suspects">Misstänkta</p>
+        <p class="nav-icon-name text-suspects"></p>
       </li>
       <li class="nav-clues nav-box" onclick="RenderClues()">
         <i class="fa-solid fa-clipboard-question"></i>
-        <p class="nav-icon-name text-clue">Ledtrådar</p>
+        <p class="nav-icon-name text-clue"></p>
       </li>
       <li class="nav-arrest nav-box" onclick="RenderMakeArrest()">
         <i class="fa-solid fa-handcuffs"></i>
-        <p class="nav-icon-name text-arrest">Arrestera</p>
+        <p class="nav-icon-name text-arrest"></p>
         </li>
     </ul>
   `;
